eaba50ea77af050f3ca1c9f84c9fd2f28b5a7708
Carlos José Mendes <87237485+MendesCJ@users.noreply.github.com>
2021-07-11 13:08:26 -0300

    Atualização
    Incluindo sons de colisão

--- Snake.py
+++ Snake.py
@@ -19,28 +19,34 @@ VERDE= (0, 100, 0)
 BRANCO = (255, 255, 255)
 VERMELHO = (255, 0, 0)
 PRETO = (0, 0, 0)
+fps = 10
 relogio = pygame.time.Clock()
 pontos = 0
 Xc = 0
 Yc = 0
 
 pygame.font.init()
+pygame.mixer.init()
 fonte = pygame.font.get_default_font()
 fontepontos = pygame.font.SysFont(fonte, 40, bold = True)
+fonteveloc = pygame.font.SysFont(fonte, 40, bold = True)
 fontegameover = pygame.font.SysFont(fonte, 60, bold = True)
+fontecontinuar = pygame.font.SysFont(fonte, 30, bold = False)
+somcolide = pygame.mixer.Sound('Plank.ogg')
+somveloc = pygame.mixer.Sound('bing.ogg')
 
 #Definições da Cobrinha
-St = 10 #Snake tamanho
+St = 20 #Snake tamanho
 Sx = random.randrange(0, LTela, St) #Snake x
 Sy = random.randrange(0, ATela, St) #Snake y
 Sc = VERDE #Snake Cor
-Sv = 10 #Snake velocidade
+Sv = 20 #Snake velocidade
 Sd = 0 #Snake direção
 Sh = [] #Snake head
 Sb = [] #Snake body
 
 #Definições da comida
-Ft = 10 #Food tamanho
+Ft = 20 #Food tamanho
 Fx = random.randrange(0, LTela, Ft) #Food x
 Fy = random.randrange(0, ATela, Ft) #Food y
 Fc = VERMELHO #Food cor
@@ -51,14 +57,16 @@ pygame.display.set_caption('Snake')
 Xc = Sv
 
 while True:
-	
-	relogio.tick(15)
+	relogio.tick(fps)
 	tela.fill(BRANCO)
 	Tpontos = f"Pontuação: {pontos}"
+	Tvelocidade = f"Velocidade: {fps}"
 	Tgameover = "Game Over"
-	TContinuar = "Deseja continuar: S ou s"
+	Tcontinuar = "Tecle s para continuar ou qualquer outra para sair"
 	texto = fontepontos.render(Tpontos, 1, PRETO)
+	texto2 = fonteveloc.render(Tvelocidade, 1, PRETO)
 	tela.blit(texto, (20, 500))
+	tela.blit(texto2, (20, 550))
 	
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -110,29 +118,55 @@ while True:
 		Fx = random.randrange(0, LTela, Ft)
 		Fy = random.randrange(0, ATela, Ft)
 		pygame.draw.rect(tela,Fc,(Fx,Fy,Ft,Ft))
+		somcolide.play()
 		pontos += 1
+		if pontos % 10 == 0:
+			somveloc.play()
+			fps += 1
 
 		
-	Sh=[]
+	Sh = []
 	Sh.append(Sx)
 	Sh.append(Sy)
 	Sb.append(Sh)
 	
 	if Sb.count(Sh) > 1:
-		exit()
+		
+		Sh = []
+		
+		while Sh == []:
+			texto = fontepontos.render(Tgameover, 1, VERMELHO)
+			tela.blit(texto, (240, 210))
+			texto2 = fontecontinuar.render(Tcontinuar, 1, VERDE)
+			tela.blit(texto2, (70, 250))
+					
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					pygame.quit()
+					exit()
+				if event.type == KEYDOWN:
+					if event.key == K_s:
+						pontos=0
+						Sh.append(Sx)
+						Sh.append(Sy)
+						Sb=[]
+					else:
+						pygame.quit()
+						exit()
+			pygame.display.update()
 	
 	
 		
 	
 	pygame.draw.rect(tela, Sc, (Sh[0], Sh[1], St, St))
-	
+		
 	for i in Sb:
 		pygame.draw.rect(tela, Sc, (i[0], i[1], St, St))
 		
 	pygame.draw.rect(tela, Fc, (Fx,Fy,Ft,Ft))
-	
+			
 	if len(Sb) > pontos:
-		del Sb[0]	
-		
+		del Sb[0]
+	
 	pygame.display.update()
 	
--- readme.md
+++ readme.md
@@ -4,12 +4,14 @@ Objetivo
 	Estudar a biblioteca pygame
 	
 Passos
-	[ok]]1º Criar tela do jogo
-	[ok]2º Criar pontos aleatóreos para a posição da cobrinha e da comida
-	[OK]3º Adicionar movimento na cobrinha
-	[OK]4º Detectar colisão com comida
-	[ok]5º Crescer conforme come
-	[Ok]6º Detectar colisão com proprio corpo
-	[Ok]7º Criar pontuação
-	[  ]8º Criar ranking.
-	[  ]9º Adicionar Sons
+	[Ok]]1º Criar tela do jogo
+	[Ok]2º 	Criar pontos aleatóreos para a posição da cobrinha e da comida
+	[OK]3º 	Adicionar movimento na cobrinha
+	[OK]4º 	Detectar colisão com comida
+	[ok]5º 	Crescer conforme come
+	[Ok]6º 	Detectar colisão com proprio corpo
+	[Ok]7º 	Criar pontuação
+	[Ok]8º 	Aumentar velocidade a cada 10 pontos
+	[Ok]9º 	Adicionar Sons
+	[  ]10º Criar ranking
+	[  ]11º Refatorar o código
